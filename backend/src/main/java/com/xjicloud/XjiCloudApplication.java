8cbbc5dced35911001b9ee34136986afad56ae65
QBM <2154424568@qq.com>
2026-07-07 22:14:03 +0800

    123

--- backend/src/main/java/com/xjicloud/XjiCloudApplication.java
+++ backend/src/main/java/com/xjicloud/XjiCloudApplication.java
@@ -1,7 +1,12 @@
 package com.xjicloud;
 
-import com.xjicloud.config.StorageProperties;
+import com.xjicloud.config.AdminProperties;
+import com.xjicloud.config.CorsProperties;
 import com.xjicloud.config.JwtProperties;
+import com.xjicloud.config.OssProperties;
+import com.xjicloud.config.RedisQueueProperties;
+import com.xjicloud.config.StorageProperties;
+import com.xjicloud.config.WorkerProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
@@ -9,7 +14,15 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
-@EnableConfigurationProperties({JwtProperties.class, StorageProperties.class})
+@EnableConfigurationProperties({
+        JwtProperties.class,
+        StorageProperties.class,
+        CorsProperties.class,
+        OssProperties.class,
+        RedisQueueProperties.class,
+        WorkerProperties.class,
+        AdminProperties.class
+})
 public class XjiCloudApplication {
 
     public static void main(String[] args) {
